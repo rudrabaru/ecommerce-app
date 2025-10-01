4dec21a044c5d670dfd3c7151ddaaafe6e7306de
Rudra <rudrabaruwala123@gmail.com>
2025-10-01 15:24:57 +0530

    final commit - navigation & email verification

--- Modules/Admin/resources/views/users.blade.php
+++ Modules/Admin/resources/views/users.blade.php
@@ -45,6 +45,7 @@
                                     { data: 'id', name: 'id', width: '60px' },
                                     { data: 'name', name: 'name' },
                                     { data: 'email', name: 'email' },
+                                    { data: 'status', name: 'status', width: '100px' },
                                     { data: 'created_at', name: 'created_at' },
                                     { data: 'actions', name: 'actions', orderable: false, searchable: false }
                                 ]
